92c443e355a5ef207604aebb7754c377fd7848da
GulsherThePython <hassanrazaali@outlook.com>
2026-05-10 22:05:54 -0700

    Implemented basic query normalization

--- main.py
+++ main.py
@@ -3,7 +3,20 @@ import string
 
 query = input("> ")
 
-
 query = query.lower()
+
+new_query = ""
 for character in query:
-    pass
+    if character not in constants.PUNCTUATION_TO_REMOVE:
+        new_query += character
+
+query = new_query
+
+new_query = ""
+for word in query.split():
+    if word not in constants.STOP_WORDS:
+        new_query += word + " "
+
+query = new_query
+
+print(query)
