dc5a4d7b0322eb6e6f6ece3f9f3cd4d7f07180c5
GulsherThePython <HassanRazaAli@outlook.com>
2026-05-18 19:01:13 -0700

    Fixed engine.py format

--- engine.py
+++ engine.py
@@ -5,57 +5,62 @@ from proccess_notes import main as proccess_notes
 import os
 import json
 
-# Ask user if they want to proccess the notes
+def run(inp):
+    query = inp
 
-q = input("Would you like to proccess the notes? (y/n) > ")
-if q == "y":
-    proccess_notes()
+    # Process query
 
-# Get the query from the user
-inp = input("Enter the query > ")
-query = inp
+    query = Sentence(query, "query")
+    query.process_sentence()
 
-# Process query
+    # Begin searching through the notes
 
-query = Sentence(query, "query")
-query.process_sentence()
+    sentence_data_list = []
+    with open(os.path.join("notes", "sentence_data_list.txt"), "r") as f:
+        sentence_data_list = json.loads(f.read())
 
-# Begin searching through the notes
+    scores = []
+    for sentence in sentence_data_list:
+        score = 0
+        for token in query.tokens:
+            for s_token in sentence["tokens"]:
+                if s_token == token:
+                    score += 2
+                    break
+                elif is_fuzzy_match(token, s_token):
+                    score += 1
+                    break
+        
+        for bigram in query.bigrams:
+            for s_bigram in sentence["bigrams"]:
+                if bigram == s_bigram:
+                    score += 5
+                    break
+                elif bigram in s_bigram:
+                    score += 3
+                    break
+                elif s_bigram in bigram:
+                    score += 3
+                    break
+        
+        score = score / max(len(sentence["tokens"]), 1)
+        scores.append([sentence, score])
 
-sentence_data_list = []
-with open(os.path.join("notes", "sentence_data_list.txt"), "r") as f:
-    sentence_data_list = json.loads(f.read())
+    def get_score(item):
+        return item[1]
 
-scores = []
-for sentence in sentence_data_list:
-    score = 0
-    for token in query.tokens:
-        for s_token in sentence["tokens"]:
-            if s_token == token:
-                score += 2
-                break
-            elif is_fuzzy_match(token, s_token):
-                score += 1
-                break
-    
-    for bigram in query.bigrams:
-        for s_bigram in sentence["bigrams"]:
-            if bigram == s_bigram:
-                score += 5
-                break
-            elif bigram in s_bigram:
-                score += 3
-                break
-            elif s_bigram in bigram:
-                score += 3
-                break
-    
-    score = score / max(len(sentence["tokens"]), 1)
-    scores.append([sentence, score])
+    scores.sort(key=get_score, reverse=True)
 
-def get_score(item):
-    return item[1]
+    return scores
 
-scores.sort(key=get_score, reverse=True)
+if __name__ == "__main__":
+    # Ask user if they want to proccess the notes
 
-print(scores[0])
+    q = input("Would you like to proccess the notes? (y/n) > ")
+    if q == "y":
+        proccess_notes()
+
+    # Get the query from the user
+    inp = input("Enter the query > ")
+    scores = run(inp)
+    print(scores[0])
