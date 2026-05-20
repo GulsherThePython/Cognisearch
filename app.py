eeb838e822e1d70f5d8e3436f0e2d078c55c43e7
GulsherThePython <hassanrazaali@outlook.com>
2026-05-19 18:41:43 -0700

    Created the notes page, created note processing in webpage, and created user note adding ability

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, redirect, url_for, render_template, request
 from engine import run
+from pathlib import Path
+from proccess_notes import main as process_notes
 
 app = Flask(__name__)
 
@@ -22,3 +24,34 @@ def search():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/notes")
+def notes():
+    notes_path = Path("notes")
+    notes_list = []
+    for file in notes_path.iterdir():
+        if file.stem == "sentence_data_list":
+            continue
+        with open(file, "r") as f:
+            raw_contents = f.read().split("\n\n")
+            contents = []
+            for note in raw_contents:
+                if note.strip() != "":
+                    contents.append(note)
+            notes_list.append(contents)
+    
+    actual_notes_list = []
+    for note in notes_list:
+        for line in note:
+            actual_notes_list.append(line)
+        
+    return render_template("notes.html", notes=actual_notes_list)
+
+@app.route("/add_note", methods=["POST"])
+def add_note():
+    note = request.form["note"]
+    with open("notes/user_notes.txt", "a") as f:
+        f.write(note.strip() + "\n\n")
+    
+    process_notes()
+    return redirect(url_for("notes"))
